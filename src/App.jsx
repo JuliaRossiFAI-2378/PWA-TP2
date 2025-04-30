33dbc7f759f9c028ed4e8130d78a905be815a127
unknown <rossijulia1999@gmail.com>
2025-04-30 17:29:21 -0300

    <|:-)
    clown push :(

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home/Home'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import paths from './paths/paths'
+import Details from './pages/Details/Details'
 
 function App() {
   return (
