bd50471bce2709a5abdcb1eb96c6613b3cef241f
Federico Lores FAI - 2948 <federico.lores@est.fi.uncoma.edu.ar>
2025-05-03 19:16:38 -0300

    footer, escalabilidad y css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,58 @@
+import { useTranslation, Trans } from "react-i18next";
+import whatsapp from '../../assets/whatsapp-48.png'
+import linkedin from '../../assets/linkedin-48.png'
+import github from '../../assets/github-50.png'
+
 const Footer = () => {
+  const{t}=useTranslation();
     return (
-      <div className="bg-gray-700 w-full p-4">
-          <h1 className="text-md font-bold text-sky-200">soy un footer :)</h1>
-      </div>
+      <footer className="bg-gray-700 text-white py-3">
+        <div className="max-w-[95%] mx-auto px-4">
+          <div className="flex flex-col md:flex-row lg:flex-row justify-between gap-2">
+            
+            <div>
+              <h3 className="text-lg font-semibold mb-2">{t("Socials")}</h3>
+              <div className="flex space-x-4 mt-2">
+                <a href="#">
+                  <img src={whatsapp} className="w-6 h-6" />
+                </a>
+                <a href="#">
+                  <img src={github} className="w-6 h-6" />
+                </a>
+                <a href="#">
+                  <img src={linkedin} className="w-6 h-6" />
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <h5 className="text-lg font-semibold mb-2">{t("Resources")}</h5>
+              <ul className="space-y-1 text-sm">
+                <li><a href="#" className="hover:text-yellow-400">{t("API")}</a></li>
+                <li><a href="#" className="hover:text-yellow-400">{t("Documentation")}</a></li>
+              </ul>
+            </div>
+
+            <div>
+              <h5 className="text-lg font-semibold mb-2">{t("Contact us")}</h5>
+              <ul className="space-y-1 text-sm">
+                <li><a href="#" className="hover:text-yellow-400">{t("Street")} Siempreviva 123, Neuquén Capital, Q8300, Argentina</a></li>
+                <li><a href="#" className="hover:text-yellow-400">+54 299 123-4567</a></li>
+                <li><a href="#" className="hover:text-yellow-400">correo@superlegit.gov</a></li>
+              </ul>
+            </div>
+
+            
+
+            
+
+          </div>
+          {/* Línea inferior */}
+          <div className="border-t border-blue-300 mt-6 pt-4 text-xs text-center">
+            Trabajo Práctico 2 - Programacion Web Avanzada {new Date().getFullYear()}
+          </div>
+        </div>
+      </footer>
     );
   };
   
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -19,67 +19,171 @@ const Details = () =>{
     return <div className="min-h-screen flex flex-col">
         <Header />
         <Background />
-        <div className="flex-grow flex-row  w-full mx-auto border-x-2 ">
-            {pokemon != null ? 
-            <>
-            <div className="flex justify-center px-10">
-                <div className="mx-16 mt-10 basis-96">
-                    <div className="mt-4">
-                        <p><span className="font-bold text-gray-700" >{t("Name")}: </span><span className="text-shadow-lg/20">{pokemon ? pokemon.name : t("Loading...")} </span></p>
+        <div className="min-h-screen flex flex-col">
+          <div className="flex-grow w-full mx-auto">
+            {pokemon != null ? (
+              <>
+                <div className=" flex flex-col lg:flex-row justify-center px-4 lg:px-10 mx-auto max-w-80 lg:max-w-3xl">
+                  <div className="mx-4 mt-6 lg:basis-96">
+                    <div>
+                      <p>
+                        <span className="font-bold text-gray-700">{t("Name")}: </span>
+                        <span className="text-shadow-lg/20">
+                          {pokemon ? pokemon.name : t("Loading...")}
+                        </span>
+                      </p>
                     </div>
-                    <div className="mt-4">
-                        <p><span className="font-bold text-gray-700" >{t("Type")}: </span><span className="text-shadow-lg/20">{pokemon ? pokemon.types.length===1 ? t(pokemon.types[0].type.name) : t(pokemon.types[0].type.name) + "/" + t(pokemon.types[1].type.name) : t("Loading...")} </span></p>
+                    <div className="mt-2">
+                      <p>
+                        <span className="font-bold text-gray-700">{t("Type")}: </span>
+                        <span className="text-shadow-lg/20">
+                          {pokemon
+                            ? pokemon.types.length === 1
+                              ? t(pokemon.types[0].type.name)
+                              : t(pokemon.types[0].type.name) +
+                                "/" +
+                                t(pokemon.types[1].type.name)
+                            : t("Loading...")}
+                        </span>
+                      </p>
                     </div>
-                    <div className="mt-4">
-                        <p><span className="font-bold text-gray-700" >{t("Height")}: </span><span className="text-shadow-lg/20">{pokemon ? pokemon.height/10 : t("Loading...")}mts </span></p>
+                    <div className="mt-2">
+                      <p>
+                        <span className="font-bold text-gray-700">{t("Height")}: </span>
+                        <span className="text-shadow-lg/20">
+                          {pokemon ? pokemon.height / 10 : t("Loading...")}mts
+                        </span>
+                      </p>
                     </div>
-                    <div className="mt-4">
-                        <p><span className="font-bold text-gray-700" >{t("Weight")}: </span><span className="text-shadow-lg/20">{pokemon ? pokemon.weight/10 : t("Loading...")}kgs </span></p>
+                    <div className="mt-2">
+                      <p>
+                        <span className="font-bold text-gray-700">{t("Weight")}: </span>
+                        <span className="text-shadow-lg/20">
+                          {pokemon ? pokemon.weight / 10 : t("Loading...")}kgs
+                        </span>
+                      </p>
                     </div>
-                    <div className="mt-4">
-                        <p><span className="font-bold text-gray-700" >{t("Abilities")}: </span><span className="text-shadow-lg/20">{pokemon ? pokemon.abilities.length===1 ? t(pokemon.abilities[0].ability.name) : pokemon.abilities[1].is_hidden === true ? t(pokemon.abilities[0].ability.name) : t(pokemon.abilities[0].ability.name) + " / " + t(pokemon.abilities[1].ability.name) : t("Loading...")} </span></p>
+                    <div className="mt-2">
+                      <p>
+                        <span className="font-bold text-gray-700">
+                          {t("Abilities")}:{" "}
+                        </span>
+                        <span className="text-shadow-lg/20">
+                          {pokemon
+                            ? pokemon.abilities.length === 1
+                              ? t(pokemon.abilities[0].ability.name)
+                              : pokemon.abilities[1].is_hidden === true
+                              ? t(pokemon.abilities[0].ability.name)
+                              : t(pokemon.abilities[0].ability.name) +
+                                " / " +
+                                t(pokemon.abilities[1].ability.name)
+                            : t("Loading...")}
+                        </span>
+                      </p>
                     </div>
-                    <div className="mt-4">
-                        <p><span className="font-bold text-gray-700" >{t("Hidden Ability")}: </span><span className="text-shadow-lg/20">{pokemon ? pokemon.abilities.length>2 && pokemon.abilities[2].is_hidden === true ? t(pokemon.abilities[2].ability.name) : pokemon.abilities.length>1 ? pokemon.abilities[1].is_hidden === true ? t(pokemon.abilities[1].ability.name) : t("none") : t("none") : t("Loading...")} </span></p>
+                    <div className="mt-2 mb-4">
+                      <p>
+                        <span className="font-bold text-gray-700">
+                          {t("Hidden Ability")}:{" "}
+                        </span>
+                        <span className="text-shadow-lg/20">
+                          {pokemon
+                            ? pokemon.abilities.length > 2 &&
+                              pokemon.abilities[2].is_hidden === true
+                              ? t(pokemon.abilities[2].ability.name)
+                              : pokemon.abilities.length > 1 &&
+                                pokemon.abilities[1].is_hidden === true
+                              ? t(pokemon.abilities[1].ability.name)
+                              : t("none")
+                            : t("Loading...")}
+                        </span>
+                      </p>
                     </div>
+                  </div>
+
+                  <div className="mt-2 mb-6 relative bg-gray-700 w-full  lg:mx-2 lg:min-w-xs max-w-xs max-h-max shadow-md shadow-sky-700/50 rounded-xl text-lg">
+                    <img
+                      className="w-64 h-64 p-3"
+                      src={pokemon.sprites.other.home.front_default}
+                    />
+                    <img
+                      className="absolute bottom-0 right-0 p-3"
+                      src={pokemon.sprites.other.showdown.front_default}
+                    />
+                  </div>
                 </div>
-                <div className="mt-10 relative bg-gray-700 w-full md:min-w-xs mx-auto md:mx-2 lg:mx-2 lg:min-w-xs max-w-xs max-h-max shadow-lg shadow-sky-900 rounded-xl text-lg my-2">
-                    <img className="w-64 h-64 p-3" src={pokemon.sprites.other.home.front_default} />
-                    <img className="absolute bottom-0 right-0 p-3" src={pokemon.sprites.other.showdown.front_default} />
+
+
+                <div className="mx-auto mt-6 px-4 max-w-xl shadow-md shadow-sky-700/50 mb-6 bg-gray-700 rounded-md">
+                  <h3 className="text-center text-xl font-bold pt-2">{t("Base Stats")}</h3>
+                  <div className="flex flex-row justify-evenly py-4">
+                    <div className="flex flex-col justify-between text-blue-400 text-sm md:text-base lg:text-base">
+                      <div className="my-1">{t("Health Points")}:</div>
+                      <div className="my-1">{t("Attack")}:</div>
+                      <div className="my-1">{t("Defense")}:</div>
+                      <div className="my-1">{t("Special Attack")}:</div>
+                      <div className="my-1">{t("Special Defense")}:</div>
+                      <div className="my-1">{t("Speed")}:</div>
+                    </div>
+
+                    <div className="flex flex-col bg-blue-300 rounded-xl min-w-[150px] lg:min-w-[270px] md:min-w-[270px]">
+                      <div className="my-1 ">
+                        <span
+                          className="mx-2 border-2 border-green-500 bg-green-500 rounded-md sm:px-2 md:px-4"
+                          style={{
+                            paddingInline: `${pokemon.stats[0].base_stat / 2}px`,
+                          }}
+                        ></span>
+                      </div>
+                      <div className="my-1">
+                        <span
+                          className="mx-2 border-2 border-red-500 bg-red-500 rounded-md sm:px-2 md:px-4"
+                          style={{
+                            paddingInline: `${pokemon.stats[1].base_stat / 2}px`,
+                          }}
+                        ></span>
+                      </div>
+                      <div className="my-1">
+                        <span
+                          className="mx-2 border-2 border-orange-500 bg-orange-500 rounded-md sm:px-2 md:px-4"
+                          style={{
+                            paddingInline: `${pokemon.stats[2].base_stat / 2}px`,
+                          }}
+                        ></span>
+                      </div>
+                      <div className="my-1">
+                        <span
+                          className="mx-2 border-2 border-purple-500 bg-purple-500 rounded-md sm:px-2 md:px-4"
+                          style={{
+                            paddingInline: `${pokemon.stats[3].base_stat / 2}px`,
+                          }}
+                        ></span>
+                      </div>
+                      <div className="my-1">
+                        <span
+                          className="mx-2 border-2 border-blue-500 bg-blue-500 rounded-md sm:px-2 md:px-4"
+                          style={{
+                            paddingInline: `${pokemon.stats[4].base_stat / 2}px`,
+                          }}
+                        ></span>
+                      </div>
+                      <div className="my-1">
+                        <span
+                          className="mx-2 border-2 border-yellow-500 bg-yellow-500 rounded-md sm:px-2 md:px-4"
+                          style={{
+                            paddingInline: `${pokemon.stats[5].base_stat / 2}px`,
+                          }}
+                        ></span>
+                      </div>
+                    </div>
+                  </div>
+
                 </div>
-            </div>
-            <div className="mx-80 py-1 mt-10 flex justify-evenly content-center shadow-lg shadow-sky-900 mb-4 bg-gray-700 rounded-md">                                                          
-                <div className="flex-col my- flex">
-                    <div className="flex-row text-blue-400 my-1">{t("Health Points")}:</div>
-                    <div className="flex-row text-blue-400 my-1">{t("Attack")}:</div>
-                    <div className="flex-row text-blue-400 my-1">{t("Defense")}:</div>
-                    <div className="flex-row text-blue-400 my-1">{t("Special Attack")}:</div>
-                    <div className="flex-row text-blue-400 my-1">{t("Special Defense")}:</div>
-                    <div className="flex-row text-blue-400 my-1">{t("Speed")}:</div>  
-                </div>
-                <div className="flex-col flex">
-                    <div className="flex-row my-1">
-                        <span className={`mx-4 border-1 border-green-500 bg-green-500 rounded-md`} style={{paddingInline:`${pokemon.stats[0].base_stat}px`}}></span>
-                    </div>        
-                    <div className="flex-row my-1">
-                        <span className={`mx-4 border-1 border-red-500 bg-red-500 rounded-md`} style={{paddingInline:`${pokemon.stats[1].base_stat}px`}}></span>
-                    </div> 
-                    <div className="flex-row my-1">
-                        <span className={`mx-4 border-1 border-orange-500 bg-orange-500 rounded-md`} style={{paddingInline:`${pokemon.stats[2].base_stat}px`}}></span>
-                    </div>
-                    <div className="flex-row my-1">
-                        <span className={`mx-4 border-1 border-purple-500 bg-purple-500 rounded-md`} style={{paddingInline:`${pokemon.stats[3].base_stat}px`}}></span>
-                    </div>
-                    <div className="flex-row my-1">
-                        <span className={`mx-4 border-1 border-blue-500 bg-blue-500 rounded-md`} style={{paddingInline:`${pokemon.stats[4].base_stat}px`}}></span>
-                    </div>
-                    <div className="flex-row my-1">
-                        <span className={`mx-4 border-1 border-yellow-500 bg-yellow-500 rounded-md`} style={{paddingInline:`${pokemon.stats[5].base_stat}px`}}></span>
-                    </div>          
-                </div> 
-            </div>
-            </>
-            : "a"}
+              </>
+            ) : (
+              "algo salio muy mal"
+            )}
+          </div>
         </div>
         <Footer />
     </div>
